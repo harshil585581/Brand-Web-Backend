322e016abf224f73b016cafdaef168674819e5e1
harshil <harshil58558@gmail.com>
2026-02-19 21:02:32 +0530

    6r547

--- main.py
+++ main.py
@@ -137,8 +137,6 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://127.0.0.1:3000",
-        "*",
-        "*"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
